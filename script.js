8a5bc7a463177039ce965b1355befa407cd2e94c
PrajwolThapa <75750574+Prajwol2005@users.noreply.github.com>
2021-11-02 18:05:58 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,17 @@
+//Task 0
+$(".example0").hide();
+//Task 1
+
+//Task 2
+$(".button2").click(function() {
+    $(".example2-div").append("blah blah blah ");
+});
+//Task 3
+$(".button3").click(function() {
+    $(".example3-div").text("Welcome to the Code Nation Fellowship");
+});
+//Task 4
+$(".button4").click(function() {
+    $(".example4-div").css("background-color", "black");
+    $(".example4-div").css("color", "white");
+});
